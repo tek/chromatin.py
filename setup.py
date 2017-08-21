e0573304214105352dfe50e1a4c46b7fe96cc02c
Torsten Schmits <torstenschmits@gmail.com>
2017-08-21 19:28:29 +0200

    `setup.py`

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+version_parts = (0, 1, 0)
+version = '.'.join(map(str, version_parts))
+
+setup(
+    name='chromatin',
+    description='neovim python plugin manager',
+    version=version,
+    author='Torsten Schmits',
+    author_email='torstenschmits@gmail.com',
+    license='MIT',
+    url='https://github.com/tek/chromatin',
+    include_package_data=True,
+    packages=find_packages(exclude=['unit', 'unit.*', 'integration', 'integration.*']),  # type: ignore
+    install_requires=[
+        'amino>=10.4.1',
+        'ribosome>=10.7.0',
+    ],
+    tests_require=[
+        'kallikrein',
+    ],
+)
