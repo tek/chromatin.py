7ab40f7c44af0b385d96164ce3594bfe96a89c90
Torsten Schmits <torstenschmits@gmail.com>
2017-09-02 20:49:21 +0200

    0.7.2

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version_parts = (0, 7, 1)
+version_parts = (0, 7, 2)
 version = '.'.join(map(str, version_parts))
 
 setup(
@@ -14,7 +14,7 @@ setup(
     include_package_data=True,
     packages=find_packages(exclude=['unit', 'unit.*', 'integration', 'integration.*']),
     install_requires=[
-        'ribosome~=10.18.0',
+        'ribosome~=10.18.1',
     ],
     tests_require=[
         'kallikrein',
