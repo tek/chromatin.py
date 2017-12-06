f6b7749d51fa81105c5e406d8e76482d5fa594a6
Torsten Schmits <torstenschmits@gmail.com>
2017-12-06 20:29:28 +0100

    use mock io executor in spec

--- unit/stage_1_spec.py
+++ unit/stage_1_spec.py
@@ -4,15 +4,20 @@ from kallikrein import k, Expectation
 from kallikrein.matchers import contain
 
 from ribosome.test.integration.run import DispatchHelper
-from ribosome.nvim.io import NvimIOState
+from ribosome.nvim.io import NvimIOState, NS
 from ribosome.plugin_state import PluginState
+from ribosome.dispatch.data import DIO, GatherIOsDIO, GatherSubprocsDIO
+from ribosome.dispatch.execute import execute_io
+from ribosome.trans.action import TransResult, TransAction
+from ribosome.process import SubprocessResult
 
-from amino import do, Just, Map
+from amino import do, Just, Map, List, Nil, Right, Path
 from amino.do import Do
 from amino.test.spec import SpecBase
 from amino.test import temp_dir, fixture_path
 
 from chromatin import config
+from chromatin.venv import Venv, ActiveVenv
 
 name = 'flagellum'
 
@@ -33,6 +38,7 @@ class Stage1Spec(SpecBase):
             chromatin_rplugins=[dict(name=name, spec=self.spec)],
             chromatin_venv_dir=str(dir),
         )
+        venv = Venv(name, dir / name, Right(Path('/dev/null')), Right(Path('/dev/null')))
         responses_strict = Map(
             {
                 'jobstart': 3,
@@ -49,9 +55,16 @@ class Stage1Spec(SpecBase):
                 return Just([])
             else:
                 return responses_strict.lift(req).o(Just(0))
-        helper = DispatchHelper.cons(config, 'core', vars=vars, responses=responses)
+        def x_io(dio: DIO) -> NS[PluginState, TransAction]:
+            if isinstance(dio, GatherIOsDIO):
+                return NS.pure(dio.io.handle_result(List(Right(venv))))
+            elif isinstance(dio, GatherSubprocsDIO):
+                return NS.pure(TransResult((List(SubprocessResult(0, Nil, Nil, venv)), Nil)))
+            else:
+                return execute_io(dio)
+        helper = DispatchHelper.cons(config, 'core', vars=vars, responses=responses, io_executor=x_io)
         r = helper.loop('chromatin:command:stage_1').unsafe(helper.vim)
-        return k(r.data.venvs.k).must(contain(name))
+        return k(r.data.venvs.k).must(contain(name)) & k(r.data.active).must(contain(ActiveVenv(venv, 3, 1111)))
 
     def foo(self) -> Expectation:
         dir = temp_dir('rplugin', 'venv')
